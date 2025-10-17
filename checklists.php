f68fe2d015500ae731f51e24357babadda8ac189
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-10-17 10:48:05 +0100

    add php/template files

---
+++ checklists.php
@@ -0,0 +1,23 @@
+<?php include "assets/template-parts/header.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Tech Learning Companion App | Jonnie Grieve Digital Media</title>
+</head>
+<body>
+    
+    <h1>JGDM: Learning Companion App (Prototype directory)</h1>
+
+    <p>checklists.php</p>
+    
+    <script type="text/javascript" src="app.js"></script>
+</body>
+</html>
+
+<?php include "assets/template-parts/footer.php"; ?>
