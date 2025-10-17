6fce0c1f1eecec38e3b32dc929e0dacb543d7296
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-10-17 11:56:31 +0100

    edit copyright notice

--- assets/template-parts/footer.php
+++ assets/template-parts/footer.php
@@ -1,5 +1,9 @@
 <?php echo "footer.php"; ?>
 
+    <footer>
+        <p>Tech Learning Companion App by <a href="https://www.jonniegrieve.co.uk" target="_blank">JGDM</a> &copy; 2025-2026 All Rights Reserved</p>
+    </footer>
+
     <script type="text/javascript" src="app.js"></script>
 </body>
 </html>
