b8f5082df478b7e44b5c4ba701fea2aec72e8c46
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-10-20 16:15:18 +0100

    implement toggled navigation

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("app.js connected - 17-10-2025 - 10:33");
+console.log("app.js connected - 20-10-2025 - 15:58");
+
+// TODO: handle toggling of .navigation visilibity
+
+function openNav() {
+
+    let nav = document.querySelector(".page---checklists");
+    nav.style.visibility = "visible";
+    // nav.style.transition = "visibility .4s ease-in-out";
+}
+
+function closeNav() {
+
+    let nav = document.querySelector(".page---checklists");
+    nav.style.visibility = "hidden";
+    // nav.style.transition = "visibility .4s ease-in-out";
+}
