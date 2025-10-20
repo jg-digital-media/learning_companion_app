5726a3b46239d71d970ae9d41d7d418e73eab924
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-10-20 16:14:57 +0100

    build a toggled navigation

--- checklists.php
+++ checklists.php
@@ -1,7 +1,42 @@
 <?php include "assets/template-parts/header.php"; ?>
     
-    <h1>JGDM: Learning Companion App (Prototype directory)</h1>
+    <header>
 
+        <h1>JGDM: Learning Companion App (Prototype directory)</h1>
+
+        <span>By Jonnie Grieve Digital Media</span>
+
+    </header>
+
+    
+        <span onclick="openNav()">&#9776;</span>
+
+    <main class="page page---checklists" onClick="closeNav()">        
+
+        <nav class="navigation main---nav">
+
+            <div class="close_menu" role="close-menu">
+                <div class="close_inner_container">
+                X
+                </div>
+            </div>
+
+            <div class="sub---navigation">
+
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="checklists.php">Flashcards</a></li>
+                    <li><a href="checklists.php">Quizzes</a></li>
+                    <li><a href="progress.php">Progress</a></li>
+                    <li><a href="motivation.php">Motivation</a></li>
+                </ul>
+
+            </div>
+
+        </nav>
+
+    </main>
+    
     <p>checklists.php</p>    
 
 <?php include "assets/template-parts/footer.php"; ?>
