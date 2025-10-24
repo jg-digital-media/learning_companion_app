dfd0f69641828e29ae99d9c5f407153c02648184
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-10-24 11:01:07 +0100

    refactor quiz card elements

--- quiz.php
+++ quiz.php
@@ -90,7 +90,7 @@
 
                     <h2>Quiz/Trivia: HTML</h2>
 
-                    <article class="quiz quiz---code">
+                    <article class="quiz quiz---trivia">
 
                         <div class="quiz---content--container">
                                 
@@ -132,73 +132,157 @@
 
                     </article>
 
-                    <article class="quiz quiz---code">
+                    <article class="quiz quiz---code quiz---flipcard">
 
-                        <div class="quiz---icon">&#128211;</div>
-
-                        <div class="quiz---content--container">
-
-                            <p>lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-
-                        </div>
+                        <div class="quiz---flipcard--inner">
                             
-                        <div class="quiz---feedback--container">
+                            <!-- Front of Card -->
+                            <div class="quiz---flipcard--front">
+                                <div class="quiz---icon">&#128211;</div>
 
-                             Complete
-                             <a href="#" id="btn_flip" class="btn btn--flip">&nbsp;</a>
-                             <a href="">Flip</a>                    
+                                <div class="quiz---content--container">
+                                    <p><strong>Question:</strong> What does HTML stand for?</p>
+                                </div>
+                                    
+                                <div class="quiz---feedback--container">
+                                    Complete
+                                    <a href="#" id="btn_complete" class="btn btn--flip">&nbsp;</a>
+                                    <a href="#" class="quizcard---flip--btn">Flip</a>                   
+                                </div>
+                            </div>
+
+                            <!-- Back of Card -->
+                            <div class="quiz---flipcard--back">
+                                <div class="quiz---icon">&#9989;</div>
+
+                                <div class="quiz---content--container">
+                                    <p><strong>Answer:</strong> HyperText Markup Language</p>
+                                    <p>HTML is the standard markup language for creating web pages and web applications.</p>
+                                </div>
+                                    
+                                <div class="quiz---feedback--container">
+                                    Complete
+                                    <a href="#" id="btn_complete" class="btn btn--flip">&nbsp;</a>
+                                    <a href="#" class="quizcard---flip--btn">Flip Back</a>                   
+                                </div>
+                            </div>
 
                         </div>
 
                     </article> 
 
-                    <article class="quiz quiz---code backface">
+                    <article class="quiz quiz---code quiz---flipcard">
 
-                        <div class="quiz---icon">&#128211;</div>
-
-                        <div class="quiz---content--container">
-
-                            <p>Code Reviewlorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-
-                        </div>
-
-                        <!-- <div class="quiz---content--container">
-
-                            <p>Code Review: Backface Fliplorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-
-                        </div> -->
+                        <div class="quiz---flipcard--inner">
                             
-                        <div class="quiz---feedback--container">
+                            <!-- Front of Card -->
+                            <div class="quiz---flipcard--front">
+                                <div class="quiz---icon">&#128211;</div>
 
-                            Complete
-                             <a href="#" id="btn_flip" class="btn btn--flip">&nbsp;</a>
-                             <a href="">Flip</a>
+                                <div class="quiz---content--container">
+                                    <p><strong>Question:</strong> What is the correct syntax for creating a hyperlink?</p>
+                                </div>
+                                    
+                                <div class="quiz---feedback--container">
+                                    Complete
+                                    <a href="#" id="btn_complete" class="btn btn--flip">&nbsp;</a>
+                                    <a href="#" class="quizcard---flip--btn">Flip</a>
+                                </div>
+                            </div>
+
+                            <!-- Back of Card -->
+                            <div class="quiz---flipcard--back">
+                                <div class="quiz---icon">&#9989;</div>
+
+                                <div class="quiz---content--container">
+                                    <p><strong>Answer:</strong> &lt;a href="url"&gt;Link Text&lt;/a&gt;</p>
+                                    <p>The &lt;a&gt; tag defines a hyperlink, and the href attribute specifies the destination URL.</p>
+                                </div>
+                                    
+                                <div class="quiz---feedback--container">
+                                    Complete
+                                    <a href="#" id="btn_complete" class="btn btn--flip">&nbsp;</a>
+                                    <a href="#" class="quizcard---flip--btn">Flip Back</a>
+                                </div>
+                            </div>
 
                         </div>
 
                     </article> 
 
-                    <article class="quiz quiz---code backface">
+                    <article class="quiz quiz---code quiz---flipcard">
 
-                        <div class="quiz---icon">&#128211;</div>
+                        <div class="quiz---flipcard--inner">
+                            
+                            <!-- Front of Card -->
+                            <div class="quiz---flipcard--front">
+                                <div class="quiz---icon">&#128211;</div>
 
-                        <div class="quiz---content--container">
+                                <div class="quiz---content--container">
+                                    <p><strong>Question:</strong> Which HTML tag is used to define an unordered list?</p>
+                                </div>
+                                    
+                                <div class="quiz---feedback--container">
+                                    Complete
+                                    <a href="#" id="btn_complete" class="btn btn--flip">&nbsp;</a>
+                                    <a href="#" class="quizcard---flip--btn">Flip</a>
+                                </div>
+                            </div>
 
-                            <p>Code Reviewlorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            <!-- Back of Card -->
+                            <div class="quiz---flipcard--back">
+                                <div class="quiz---icon">&#9989;</div>
+
+                                <div class="quiz---content--container">
+                                    <p><strong>Answer:</strong> &lt;ul&gt;</p>
+                                    <p>The &lt;ul&gt; tag creates an unordered (bulleted) list. List items are defined with &lt;li&gt; tags.</p>
+                                </div>
+                                    
+                                <div class="quiz---feedback--container">
+                                    Complete
+                                    <a href="#" id="btn_complete" class="btn btn--flip">&nbsp;</a>
+                                    <a href="#" class="quizcard---flip--btn">Flip Back</a>
+                                </div>
+                            </div>
 
                         </div>
 
-                        <!-- <div class="quiz---content--container">
+                    </article> 
+                    
+                    <article class="quiz quiz---code quiz---flipcard">
 
-                            <p>Code Review: Backface Fliplorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-
-                        </div> -->
+                        <div class="quiz---flipcard--inner">
                             
-                        <div class="quiz---feedback--container">
+                            <!-- Front of Card -->
+                            <div class="quiz---flipcard--front">
+                                <div class="quiz---icon">&#128211;</div>
 
-                             Complete
-                             <a href="#" id="btn_flip" class="btn btn--flip">&nbsp;</a>
-                             <a href="">Flip</a>
+                                <div class="quiz---content--container">
+                                    <p><strong>Question:</strong> What is the purpose of the &lt;head&gt; element in HTML?</p>
+                                </div>
+                                    
+                                <div class="quiz---feedback--container">
+                                    Complete
+                                    <a href="#" id="btn_complete" class="btn btn--flip">&nbsp;</a>
+                                    <a href="#" class="quizcard---flip--btn">Flip</a>
+                                </div>
+                            </div>
+
+                            <!-- Back of Card -->
+                            <div class="quiz---flipcard--back">
+                                <div class="quiz---icon">&#9989;</div>
+
+                                <div class="quiz---content--container">
+                                    <p><strong>Answer:</strong> Contains metadata about the HTML document</p>
+                                    <p>The &lt;head&gt; element contains meta information like title, scripts, styles, and other metadata that isn't displayed on the page.</p>
+                                </div>
+                                    
+                                <div class="quiz---feedback--container">
+                                    Complete
+                                    <a href="#" id="btn_complete" class="btn btn--flip">&nbsp;</a>
+                                    <a href="#" class="quizcard---flip--btn">Flip Back</a>
+                                </div>
+                            </div>
 
                         </div>
 
